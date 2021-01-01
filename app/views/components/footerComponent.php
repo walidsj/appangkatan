1b6f3e3db3b5b54957af9f4a0ead0c632d107c84
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2021-01-01 12:58:35 +0700

    Update footerComponent.php

--- app/views/components/footerComponent.php
+++ app/views/components/footerComponent.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
    <div class="container my-auto">
       <div class="copyright text-center my-auto">
-         <span>Copyright &copy; 2020 <?= getenv('app.Name'); ?> | Data Independen Data Aman</span>
+         <span>Copyright &copy; 2020 <?= getenv('app.Name'); ?> | Data Independen Data Aman. Page rendered in <strong>{elapsed_time}</strong> seconds.</span>
       </div>
    </div>
 </footer>

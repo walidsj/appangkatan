5cb40263a1a82aeb1c70aa64c22d273c8a65bcef
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2021-03-10 09:44:29 +0700

    Update Data.php

--- app/controllers/Data.php
+++ app/controllers/Data.php
@@ -33,7 +33,7 @@ class Data extends CI_Controller
 		if ($idSemester) {
 			$data['matkulList'] = $this->db->where('statusMatkul', 1)->where('prodiMatkul', $this->userSession->prodiUser)->where('semesterMatkul', $idSemester)->join('ip', 'ip.matkulIp = matkul.idMatkul AND ip.userIp = ' . $this->userSession->idUser, 'left')->join('predikat', 'ip.predikatIp = predikat.idPredikat', 'left')->order_by('namaMatkul', 'ASC')->get('matkul')->result();
 		} else {
-			$semester = $this->db->where('statusSemester', 1)->where('idSemester', $this->userSession->prodiUser)->order_by('namaSemester', 'DESC')->get_where('semester')->row();
+			$semester = $this->db->where('statusSemester', 1)->where('prodiSemester', $this->userSession->prodiUser)->order_by('namaSemester', 'DESC')->get_where('semester')->row();
 
 			$data['selectedSemester'] = $semester->idSemester;
 
